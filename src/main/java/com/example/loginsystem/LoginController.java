ba7a67bb98944ba183394ba81f90c63be4954134
Shakezk <kyleshakes04@gmail.com>
2023-10-08 22:55:07 +0100

    Once button is clicked it prints the username and password that the user input

--- src/main/java/com/example/loginsystem/LoginController.java
+++ src/main/java/com/example/loginsystem/LoginController.java
@@ -1,8 +1,17 @@
 package com.example.loginsystem;
-
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
 
+
+    public void login() {
+        System.out.println(usernameField.getText());
+        System.out.println(passwordField.getText());
+    }
 }
