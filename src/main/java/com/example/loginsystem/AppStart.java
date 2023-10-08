a326f4d4e3bc3e17cb26af74955a6e5f43204db3
Shakezk <kyleshakes04@gmail.com>
2023-10-08 23:45:25 +0100

    Added a sign up page with UI

--- src/main/java/com/example/loginsystem/AppStart.java
+++ src/main/java/com/example/loginsystem/AppStart.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class AppStart extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AppStart.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AppStart.class.getResource("login-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 300);
         stage.setTitle("Login System");
         stage.setResizable(false);
--- src/main/java/com/example/loginsystem/LoginController.java
+++ src/main/java/com/example/loginsystem/LoginController.java
@@ -1,7 +1,15 @@
 package com.example.loginsystem;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 class AdminAccount{
     static String adminUsername = "admin";
@@ -14,11 +22,23 @@ public class LoginController {
     @FXML
     private PasswordField passwordField;
 
+    private Scene scene;
+    private Stage stage;
+
     public void login() {
         if (usernameField.getText().equals(AdminAccount.adminUsername) && passwordField.getText().equals(AdminAccount.adminPassword)){
             System.out.println("Admin Account");
         }else{
             System.out.println("Normal account");
         }
+    }
+
+    public void signUp(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("signUp-page.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setResizable(false);
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
