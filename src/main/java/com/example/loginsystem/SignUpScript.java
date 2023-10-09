a1815aee5bcca2429f54f50d72da89c2b19b5872
Shakezk <kyleshakes04@gmail.com>
2023-10-09 21:36:04 +0100

    Changed the last name from password field to a normal textfield and now when you sign up, it takes you back to the login page

--- src/main/java/com/example/loginsystem/SignUpScript.java
+++ src/main/java/com/example/loginsystem/SignUpScript.java
@@ -1,6 +1,24 @@
 package com.example.loginsystem;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SignUpScript {
 
-
+    private Scene scene;
+    private Stage stage;
+    public void signUp(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login-page.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setResizable(false);
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
